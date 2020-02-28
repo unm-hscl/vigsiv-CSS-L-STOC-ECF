8d6820cce9e3e81f3235b15eade0d49da81e257b
Vignesh <vigsiv@unm.edu>
2020-02-28 11:37:02 -0800

    Removed Figure 3 changed file names

--- MATLAB/src/Figure4.m
+++ MATLAB/src/Figure4.m
@@ -1,7 +1,7 @@
-%% vigsiv-CSS-L-STOC-ECF: Figure 4: Double Integrator
+%% vigsiv-CSS-L-STOC-ECF: Figure 4: Hypersonic Vehicle
 % This code runs a simple double-integrator, LTI system with UNKNOWN 
 % additive disturbance to evaluate the probability that the state will lie
-% in a specified halfspace using approximate cdfs from the 
+% in a specified envelope using approximate cdfs from the 
 % characteristic function (using CharFunTool).
 %
 % REQUIRED DEPENDENCIES: - CharFunTool 
@@ -19,63 +19,87 @@ clc, clear, close all
 width = 252; 
 height = 150;
 plot_markersize = 8;
-plot_fontSize = 9;
+plot_fontSize = 8;
 plot_linewidth = 2;
 
-%% System dynamics: 
 
-    dim = 2; 
-    prob.Ts = 0.25;
-    prob.T = 10; % time horizon
-    ulim = 0;
+prob.Ts = 0.25; % Step size
+prob.T = 10; % Time horizon
 
-    input_space = Polyhedron('lb',-ulim,'ub',-ulim);
-        disturb = RandomVector('UserDefined',...
+
+% Hypersonics_linearization
+
+% A = prob.Ts*A+eye(5);
+% B = prob.Ts*B;
+
+A = [0  0        -7702.0808 7702.0808 0;
+     0 -0.001346  21.284    -32.2     0;
+     0 -9.919E-7  -0.0696    0        1;
+     0  0         0          0        1;
+     0 -2.08E-6   2.945      0        0];
+ 
+B = [0          0;
+     24.43 -37.19    ;
+     -8.385E-5 -0.01121; 
+     0         0;
+     .123 -1.49];
+ 
+     
+sysC = ss(A,B,zeros(1,5),0); % setup a state-space model
+sysD = c2d(sysC,prob.Ts); % discretize the dynamics
+Adis = sysD.A;
+Bdis = sysD.B; 
+     
+% Distrubance matrix (12x3 matrix): 
+% 
+% Note that there is only a disturbance on the states for the position
+% of the quadcopter. 
+
+    G = [1 0;0 1; 0 0; 0 0; 0 0];
+     
+     
+ % Defining the disturbance: 
+    
+    
+    dist = RandomVector('UserDefined',...
                         @(N) [zeros(2,N)]);
-    sys = getChainOfIntegLtiSystem(2, prob.Ts, input_space, disturb);
-    sys_lti_no_input = LtiSystem('StateMatrix', sys.state_mat,...
-                'DisturbanceMatrix', sys.dist_mat,...
-                'Disturbance', sys.dist);
+     
+                         
+                   
+    sys_lti = LtiSystem('StateMatrix', Adis,...,
+                'InputMatrix',Bdis,...
+                'InputSpace', Polyhedron('lb',[-ones(2,1)],...
+                'ub', [ones(2,1)]),...
+                'DisturbanceMatrix', G,...
+                'Disturbance', dist);
             
-    % Generate concat matrices for evaluating the reach probability by
-    % characteristic functions. 
+            
+    
+% Generate concat matrices 
 
-    [prob.Ad,...
-        prob.Bd,...
-            prob.Gd] = getConcatMats(sys, prob.T); % Generate concat matrices.
+    [prob.Ad, prob.Bd, prob.Gd] = getConcatMats(sys_lti, prob.T); 
+    
     
 % Generate data vector: 
     
 n = 1000;
-for i = 1:size(prob.Gd(:,1:dim:end),2)
-%     data(2*i-1:2*i,:) = [normrnd(0,1,n,1)';normrnd(0,0.075,n,1)';];
-%       data(i,:) = [exprnd(1,n,1);];
-%     data(2*i-1:2*i,:) = [exprnd(2,n,1)'; normrnd(1,0.05,n,1)'];
-%     data(2*i-1:2*i,:) = [gamrnd(0.5,1,[n,1])'; unifrnd(-0.05,0.05,n,1)'];
-    data(2*i-1:2*i,:) = [unifrnd(-5,5,n,1)';0.005*gamrnd(8,0.5,[n,1])'; ];
-%     data(i,:) = [gamrnd(9,0.5,[n,1]); unifrnd(0,10,n,1);];
-%     data(i,:) = [gamrnd(8,0.5,[n,1]); exprnd(1,n,1);];
-%     data = repmat(data,size(Gd,2));
+for i = 1:size(prob.Gd(:,1:2:end),2)
+    data(2*i-1:2*i,:) = [2*wblrnd(5,4,[n,1])'; gamrnd(5,1,n,1)'];
     for j = 2*i-1:2*i
     [sigma(j),~,~,~] = kde(data(j,:),n,min(data(j,:)),max(data(j,:)));
     end
 end
 
 prob.N = 50;
-for i = 1:size(prob.Gd(:,1:dim:end),2)
-%     data(2*i-1:2*i,:) = [normrnd(0,1,n,1)';normrnd(0,0.075,n,1)';];
-%       data(i,:) = [exprnd(1,n,1);];
-%     data(2*i-1:2*i,:) = [exprnd(2,n,1)'; normrnd(1,0.05,n,1)'];
-%     prob.realizations(2*i-1:2*i,:) = [gamrnd(0.5,1,[prob.N,1])'; unifrnd(-0.05,0.05,prob.N,1)'];
-    prob.realizations(2*i-1:2*i,:) = [unifrnd(-5,5,prob.N,1)';0.005*gamrnd(8,0.5,[prob.N,1])'; ];
-%     data(i,:) = [gamrnd(9,0.5,[n,1]); unifrnd(0,10,n,1);];
-%     data(i,:) = [gamrnd(8,0.5,[n,1]); exprnd(1,n,1);];
-%     data = repmat(data,size(Gd,2));
+for i = 1:size(prob.Gd(:,1:2:end),2)
+    prob.realizations(2*i-1:2*i,:) = [2*wblrnd(5,4,[prob.N,1])'; gamrnd(5,1,prob.N,1)'];
+
 end
+
 
 % Compute upper and lower bounds on the mean: 
 
-for i = 1:size(data,1)
+for i = 1:size(prob.Gd,1)
     
    
     cf_func_c = @(t) diracMixtureICC(t,prob.Gd(i,:)*data,prob.Gd(i,:)*diag(sigma)*prob.Gd(i,:)');
@@ -112,35 +136,51 @@ for i = 1:size(data,1)
 %     plot(xc{i},cdfu{i},'-r','Linewidth',2)
     
 end
+    
+    
+% Compute the cdf using the Gil-Pelaez inversion formula: 
 
-% Generate the characteristic function to invert. 
+    % Specify safe set: 
 
-    % Bounds on the safe set: 
+    p = [-1 zeros(1,4);
+          1 zeros(1,4);
+          0 -1  0 0 0;
+          0  1  0 0 0;];
+    q = [-85000 85200 -7650 7750]';
+    
+%     p = [-1  0 0   0 0;
+%           1  -1 0   0 0;
+%           0  1 -1 0 0;
+%           0  0  1 0 0;];
+%     q = [84800 85200 deg2rad(10) deg2rad(10)]';
 
-    p = [1 0; -1 0;];
-    q = linspace(50,30, prob.T);
+    pbig = kron(eye(prob.T),p);
+    qbig = repmat(q,prob.T,1);
 
-    % Generate bounds for Ono and PWA: 
-    prob.pbig = kron(eye(prob.T),p);
-    prob.qbig = kron(q,[1,1])';
+    prob.pbig = pbig; 
+    prob.qbig = qbig;
 
-    n_lin_const = size(prob.pbig,1);
+    
+    n_lin_const = size(pbig,1); 
+
+
 disp('Generating PWAUA for ICC')    
 tic    
 for k = 1:n_lin_const
                 
                 transformed_rv = prob.pbig(k,:)*prob.Gd*data;
-%                 [sigma(k),~,~,~] =kde(transformed_rv,n,min(transformed_rv),max(transformed_rv));
+                
                 cf_func = @(t) diracMixtureICC(t,transformed_rv,(prob.pbig(k,:)*prob.Gd)*diag(sigma)*(prob.pbig(k,:)*prob.Gd)');
                 clear options
                 options.isPlot = false;
-                options.xN = 1000;
+                options.xN = 1000; 
                 options.xMin = min(transformed_rv); 
                 options.xMax = max(transformed_rv);
 %                 options.N = 1000;
                 result{k} = cf2DistGP(cf_func,[],[],options);
                 
                 x{k} = fliplr(result{1,k}.x)';
+                pdf{k} = fliplr(result{1,k}.pdf)';
                 cdf{k} = fliplr(result{1,k}.cdf)';
                 
 %                 x{k} = result{1,k}.x';
@@ -157,7 +197,7 @@ for k = 1:n_lin_const
                 
                 y{k} = min(prob.pu_m{k}.*x{k}(xind:end)+prob.pu_c{k},[],2);
                     
-%                 figure(2)
+%                 figure
 %                 title('CDF')
 %                 hold on
 %                 empcdf = histogram(transformed_rv,'Normalization','cdf');
@@ -174,24 +214,36 @@ toc
 %% Problem params: 
 
 prob.Delta = 1-0.8;
-prob.x0 = [0; 0];
-Xd = linspace(-50,-50,prob.T)';
-prob.Xd = reshape([Xd'; zeros(size(Xd'))],[],1);
-prob.ulimu = 100;
-prob.uliml = -100;
+prob.x0 = [85000; 7702.0808;deg2rad(1.5153);deg2rad(1.5153);0;];
+Xd1 = linspace(85000,85000,prob.T)';
+Xd2 = linspace(7702.0808,7702.0808,prob.T)';
+Xd3 = linspace(deg2rad(1.5153),deg2rad(1.5153),prob.T)';
+Xd4 = linspace(deg2rad(1.5153),deg2rad(1.5153),prob.T)';
+Xd5 = linspace(0,0,prob.T)';
+prob.Xd = reshape([Xd1'; Xd2';Xd3';Xd4';Xd5'],[],1);
+prob.ulimu = repmat([1.2; 0.2618],prob.T,1);
+prob.uliml = repmat([0.2; -0.2618],prob.T,1);
 prob.xlb = res(:,1);
 
+% prob.xterm = prob.x0;
+
 %% Generate Moments for the Quadratic Cost: 
-prob.R = kron(eye(prob.T),0.01);
-prob.Q = 10*eye(size(prob.Gd,2));
+prob.R = 1E-2*eye(size(prob.Bd,2));
+prob.Q = 10*eye(size(prob.Ad,1));
+% prob.Q(end-4:end,end-4:end) = 1000*prob.Q(end-4:end,end-4:end);
 prob.D = chol(prob.Q);
+
+for i = 1:size(data,1)
+    [sigma2(i,:),~,~,~] =kde(data(i,:),size(data,2),...
+        min(data(i,:)),max(data(i,:)));
+end
 
 disp('Computing Moments for Cost')
 tic
 % E[w] -- mean
 prob.muvec =  diracMixtureCostmean(data);
 % E[W'W]
-m2 = diracMixtureCostcov(data,sigma');
+m2 = diracMixtureCostcov(data,sigma2);
 prob.muvec2 = m2- prob.muvec.^2;
 toc
 
@@ -208,6 +260,7 @@ for run_indx = 1:3
 end
 
 blackmore_mean_time = sum(blackmore_time_to_solve)/length(blackmore_time_to_solve);
+
 
 %% Monte-Carlo Validation
         n_mcarlo_sims = 1e5;  
@@ -232,12 +285,12 @@ for input_vec_indx = 1:size(collection_of_input_vectors,2)
     true_cost = collection_of_costs(input_vec_indx);
     % This function returns the concatenated state vector stacked columnwise
     n = n_mcarlo_sims;
-    for i = 1:size(prob.Gd(:,1:dim:end),2)
+    for i = 1:size(prob.Gd(:,1:2:end),2)
 %         Wvec(2*i-1:2*i,:) = [normrnd(0,1,n,1)';normrnd(0,0.075,n,1)';];
     %     data(i,:) = [exprnd(1,n,1);];
 %         Wvec(2*i-1:2*i,:) = [exprnd(2,n,1)'; normrnd(1,0.05,n,1)'];
 %         Wvec(2*i-1:2*i,:) = [gamrnd(0.5,1,[n,1])'; unifrnd(-0.05,0.05,n,1)'];
-        Wvec(2*i-1:2*i,:) = [unifrnd(-5,5,n,1)';0.005*gamrnd(8,0.5,[n,1])'; ];
+        Wvec(2*i-1:2*i,:) = [2*wblrnd(5,4,[n,1])'; gamrnd(5,1,n,1)'];
     %     data(i,:) = [gamrnd(9,0.5,[n,1]); unifrnd(0,10,n,1);];
     %     data(i,:) = [gamrnd(8,0.5,[n,1]); exprnd(1,n,1);];
     %     data = repmat(data,size(Gd,2));
@@ -265,52 +318,77 @@ for input_vec_indx = 1:size(collection_of_input_vectors,2)
 end
 
 
+
 %% Plotting
 
 Fig4 = figure('Units', 'points', ...
                 'Position', [0, 0, width, height]); 
-polyvertex =[0:prob.T,0:prob.T;[-prob.qbig(1),-prob.qbig(1:2:end)'],...
-        [prob.qbig(1),prob.qbig(1:2:end)']]'; % Note: This needs MPT to run!!
-P = Polyhedron('V',polyvertex);
+
+subplot(2,1,1)
 hold on
-h1 = plot(P,'alpha',0.1);
+h1 = yline(-q(1),'r','LineWidth',plot_linewidth);
+yline(q(2),'r','LineWidth',plot_linewidth)
 h11 = plot(0,prob.x0(1),'.b','MarkerSize',plot_markersize);
-h2 = plot(1:(prob.T),Xd(1:end),'go','MarkerSize',...
+h2 = plot(1:(prob.T),prob.Xd(1:5:end),'go','MarkerSize',...
     plot_markersize,'LineWidth',2);
-h3 = plot(1:(prob.T),ECFSTOC_opt_mean_X(1:2:end),'md',...
+h3 = plot(1:(prob.T),ECFSTOC_opt_mean_X(1:5:end),'md',...
     'LineWidth',1,'MarkerSize',plot_markersize);
 xl = prob.Ad*prob.x0+prob.Bd*ECFSTOC_opt_input_vector+GdWl';
 xu = prob.Ad*prob.x0+prob.Bd*ECFSTOC_opt_input_vector+GdWu';
 xm = prob.Ad*prob.x0+prob.Bd*ECFSTOC_opt_input_vector+prob.Gd*Wvec;
-xlp = plot(1:(prob.T),xl(1:2:end),'r','LineWidth',1);
-xup = plot(1:(prob.T),xu(1:2:end),'r','LineWidth',1);
-% for h = 1:n_mcarlo_sims/100
-%     xmp = plot(2:(prob.T+1),xm(1:2:end,h));
-% end
-h4 = plot(1:(prob.T),blackmore_opt_mean_X(1:2:end,1),...
+xlp = plot(1:(prob.T),xl(1:5:end),'r','LineWidth',1);
+xup = plot(1:(prob.T),xu(1:5:end),'r','LineWidth',1);
+h4 = plot(1:(prob.T),blackmore_opt_mean_X(1:5:end,1),...
     'ks','MarkerSize',plot_markersize);
 
+set(groot, 'defaultAxesTickLabelInterpreter','latex'); 
+set(groot, 'defaultLegendInterpreter','latex');
+set(groot, 'defaulttextInterpreter','latex');
+
+% xlabel('Time Step, k')
+ylabel('Altitude, ft')
+% legend([h1 h11 h2 h3 h4],{'Target Tube',...
+%     'Initial state','Target Trajectory',...
+%     'ECF Stochastic Optimal Control',...
+%     sprintf('Particle control (PC), %i Particles',prob.N)},...
+%     'Location','southoutside','FontSize',plot_fontSize);
+box on;
+set(gca,'FontSize',plot_fontSize);
+set(gca,'xtick',[]);
+axis([0 10 84900 85200])
+
+subplot(2,1,2)
+hold on
+h1 = yline(-q(3),'r','LineWidth',plot_linewidth);
+yline(q(4),'r','LineWidth',plot_linewidth)
+h11 = plot(0,prob.x0(2),'.b','MarkerSize',plot_markersize);
+h2 = plot(1:(prob.T),prob.Xd(2:5:end),'go','MarkerSize',...
+    plot_markersize,'LineWidth',2);
+h3 = plot(1:prob.T,ECFSTOC_opt_mean_X(2:5:end),'md',...
+    'LineWidth',1,'MarkerSize',plot_markersize);
+xl = prob.Ad*prob.x0+prob.Bd*ECFSTOC_opt_input_vector+GdWl';
+xu = prob.Ad*prob.x0+prob.Bd*ECFSTOC_opt_input_vector+GdWu';
+xm = prob.Ad*prob.x0+prob.Bd*ECFSTOC_opt_input_vector+prob.Gd*Wvec;
+xlp = plot(1:(prob.T),xl(2:5:end),'r','LineWidth',1);
+xup = plot(1:(prob.T),xu(2:5:end),'r','LineWidth',1);
+h4 = plot(1:(prob.T),blackmore_opt_mean_X(2:5:end,1),...
+    'ks','MarkerSize',plot_markersize);
 
 set(groot, 'defaultAxesTickLabelInterpreter','latex'); 
 set(groot, 'defaultLegendInterpreter','latex');
 set(groot, 'defaulttextInterpreter','latex');
 
 xlabel('Time Step, k')
-ylabel('Position, x')
+ylabel('Velocity, ft/s')
+axis([0 10 7650 7750])
 legend([h1 h11 h2 h3 xlp h4],{'Safety boundary',...
-    'Initial state','Target trajectory',...
-    'Algorithm 2',...
+    'Initial state','Target Trajectory',...
+    'Algortihm 2',...
     'Confidence interval',...
     sprintf('Particle control, %i Particles',prob.N)},...
-    'Location','southoutside','FontSize',plot_fontSize);
+    'Location','eastoutside','FontSize',plot_fontSize);
 box on;
 set(gca,'FontSize',plot_fontSize);
-
-
-
-
-
-
 
 function m = diracMixtureCostmean(data)
 
